86f542d2bc4a0d3d08e1d003bfd5e2b7c3966b7d
minhdangv <dangvuongminh25@gmail.com>
2022-10-15 20:57:46 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,12 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+        
 ];
 
 const router = createRouter({
